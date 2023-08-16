faaa2b2962bdee4fc43f41053d585fc0349cafa5
Alexander Sakhaev <alsakhaev@gmail.com>
2023-08-16 20:30:04 +0300

    feat: introduce dapplet executor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { BrowserOptions } from "./fixtures/browser";
 import { test, expect } from "./fixtures/dapplet-runner";
+import * as DappletExecutor from "./fixtures/dapplet-executor";
 
-export { test, expect, BrowserOptions };
+export { test, expect, BrowserOptions, DappletExecutor };
